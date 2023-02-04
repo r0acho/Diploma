6b2a4d21a91ad32466dfcb927de9af02e8464b03
Ivan L <ivan.linzov@gmail.com>
2023-02-04 23:27:20 +0300

    устранен баг с ошибкой платежа

--- Diploma/Data/Models/PaymentInfo.cs
+++ Diploma/Data/Models/PaymentInfo.cs
@@ -26,22 +26,30 @@ namespace Diploma.Data.Models
         /// <summary>
         /// JSON для тестовой модели
         /// </summary>
-        static string testJson = $"{{\r\n  \"AMOUNT\": 300.2,\r\n  \"CURRENCY\": \"RUB\",\r\n  \"ORDER\": 123,\r\n  \"DESC\": \"Test Payment\",\r\n  \"TERMINAL\": \"79036777\",\r\n  \"TRTYPE\": 1,\r\n  \"MERCH_NAME\": \"Test Shop\",\r\n  \"MERCHANT\": \"000599979036777\",\r\n  \"EMAIL\": \"cardholder@mail.test\",\r\n  \"TIMESTAMP\": 20230204120000,\r\n  \"NONCE\": \"0xf323f1fad21ab156\",\r\n  \"BACKREF\": \"https://localhost:7269/index\",\r\n  \"NOTIFY_REF\": \"https://localhost:7269/index\",\r\n  \"CALDHOLDER_NOTIFY\": \"EMAIL\",\r\n  \"MERCHANT_NOTIFY\": \"EMAIL\",\r\n  \"MERCHANT_NOTIFY_EMAIL\": \"merchant@mail.test\"\r\n}}";
+        static string testJson = $"{{\r\n  \"AMOUNT\": 300.2,\r\n  " +
+            $"\"CURRENCY\": \"RUB\",\r\n  \"ORDER\": \"{Random.Shared.Next(10000000, 90000000)}\",\r\n " +
+            $" \"DESC\": \"Test Payment\",\r\n  \"TERMINAL\": \"79036777\",\r\n  " +
+            $"\"TRTYPE\": 1,\r\n  \"MERCH_NAME\": \"Test Shop\",\r\n  \"MERCHANT\": " +
+            $"\"000599979036777\",\r\n  \"EMAIL\": \"cardholder@mail.test\",\r\n  " +
+            $"\"TIMESTAMP\": {DateTime.UtcNow.ToString("yyyyMMddHHmmss")},\r\n  \"NONCE\": \"8b495c3669edb02003c2dca666d2182a\"," +
+            $"\r\n  \"BACKREF\": \"https://localhost:7269/index\",\r\n  \"NOTIFY_REF\": " +
+            $"\"https://localhost:7269/index\",\r\n  \"CALDHOLDER_NOTIFY\": \"EMAIL\",\r\n  " +
+            $"\"MERCHANT_NOTIFY\": \"EMAIL\",\r\n  \"MERCHANT_NOTIFY_EMAIL\": \"merchant@mail.test\"\r\n}}";
 
         /// <summary>
         /// Поля, которые нужно отправить для проведения транзакции
         /// </summary>
         public static readonly List<string> RequestKeys = new List<string> {
-        "AMOUNT", "CURRENCY", "ORDER", "DESC", "TERMINAL", "TRTYPE","MERCH_NAME",
-        "MERCHANT","EMAIL", "TIMESTAMP","NONCE","BACKREF","NOTIFY_URL","P_SIGN"
+            "AMOUNT", "CURRENCY", "ORDER", "DESC", "TERMINAL", "TRTYPE","MERCH_NAME",
+            "MERCHANT","EMAIL", "TIMESTAMP","NONCE","BACKREF","NOTIFY_URL","P_SIGN"
         };
 
         /// <summary>
         /// Порядок полей для вычисления параметра P_SIGN
         /// </summary>
         public static readonly List<string> PSignOrder = new List<string> {
-        "AMOUNT", "CURRENCY", "ORDER", "MERCH_NAME", "MERCHANT",
-        "TERMINAL", "EMAIL", "TRTYPE", "TIMESTAMP", "NONCE", "BACKREF"
+            "AMOUNT", "CURRENCY", "ORDER", "MERCH_NAME", "MERCHANT",
+            "TERMINAL", "EMAIL", "TRTYPE", "TIMESTAMP", "NONCE", "BACKREF"
         };
 
         /// <summary>
