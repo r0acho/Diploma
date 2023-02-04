0a4052381a4b8d8116513952203fce7ba9c7f729
Ivan L <ivan.linzov@gmail.com>
2023-02-04 17:36:05 +0300

    Реализован статический класс Enviroment, в котором хранятся общие данные для взаимодействия модуля с банком

--- Diploma/Data/Models/Enviroment.cs
+++ Diploma/Data/Models/Enviroment.cs
@@ -1,6 +1,35 @@
 ﻿namespace Diploma.Data.Models
 {
-    public class Enviroment
+    /// <summary>
+    /// Класс для хранения всего, что переедет в переменные окружения (или в хранилище секретов ASP.NET)
+    /// Пока храню информацию, которую не страшно показать :)
+    /// </summary>
+    public static class Enviroment
     {
+        /// <summary>
+        /// Первая компонента ключа
+        /// </summary>
+        static byte[] comp1 = Convert.FromHexString("C50E41160302E0F5D6D59F1AA3925C45");
+
+        /// <summary>
+        /// Вторая компонента ключа
+        /// </summary>
+        static byte[] comp2 = Convert.FromHexString("00000000000000000000000000000000");
+
+
+        /// <summary>
+        /// Секретный ключ
+        /// </summary>
+        static byte[] secretKey;
+
+        /// <summary>
+        /// URL ПШ для тестовой среды
+        /// </summary>
+        static string bankUrl = "https://test.3ds.payment.ru/cgi-bin/cgi_link";
+
+        static Enviroment()
+        {
+            secretKey = comp1.Zip(comp2, (a, b) => (byte)(a ^ b)).ToArray();
+        }
     }
 }
